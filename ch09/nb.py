91f116b522a9b279c8aa5d3a4be6fb8d5fd4b71e
YuanchenHua <657223675@qq.com>
2019-12-06 16:59:15 +0800

    finished ch08,ch09

--- ch09/nb.py
+++ ch09/nb.py
@@ -1,10 +1,6 @@
-#%%
-import tensorflow as tf 
-from    tensorflow.keras import layers
+import tensorflow as tf
+from tensorflow.keras import layers
 
-pip install -U scikit-learn
-
-#%%
 # 添加dropout操作
 x = tf.nn.dropout(x, rate=0.5)
 # 添加Dropout层
@@ -14,23 +10,17 @@ model.add(layers.Dropout(rate=0.5))
 loss_reg = lambda_ * tf.reduce_sum(tf.square(w))
 # 在层方式时添加范数函数
 Dense(256, activation='relu',
-                    kernel_regularizer=regularizers.l2(_lambda))
+      kernel_regularizer=regularizers.l2(_lambda))
 
-#%%
-#                     
 # 创建网络参数w1,w2
-w1 = tf.random.normal([4,3])
-w2 = tf.random.normal([4,2])
+w1 = tf.random.normal([4, 3])
+w2 = tf.random.normal([4, 2])
 # 计算L1正则化项
-loss_reg = tf.reduce_sum(tf.math.abs(w1))\
-    + tf.reduce_sum(tf.math.abs(w2))
-
+loss_reg = tf.reduce_sum(tf.math.abs(w1)) \
+           + tf.reduce_sum(tf.math.abs(w2))
 
 # 计算L2正则化项
-loss_reg = tf.reduce_sum(tf.square(w1))\
-    + tf.reduce_sum(tf.square(w2))
+loss_reg = tf.reduce_sum(tf.square(w1)) \
+           + tf.reduce_sum(tf.square(w2))
 
-#%%
 loss_reg
-
-#%%
