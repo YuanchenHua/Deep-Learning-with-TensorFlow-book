740a4eece2c4167b485a146cf7b3f6c6a22092ac
YuanchenHua <657223675@qq.com>
2019-12-19 09:18:21 +0800

    add some c

--- ch08/nb.py
+++ ch08/nb.py
@@ -9,6 +9,7 @@ x = tf.constant([2.,1.,0.1])
 layer = layers.Softmax(axis=-1)
 layer(x)
 #%%
+# 这一步相当于把x reshape成 一维数据
 def proprocess(x,y):
     x = tf.reshape(x, [-1]) 
     return x,y
@@ -17,6 +18,7 @@ def proprocess(x,y):
 # y: [60k]
 (x, y), (x_test,y_test) = datasets.mnist.load_data()
 # x: [0~255] => [0~1.]
+# 转换成tensor
 x = tf.convert_to_tensor(x, dtype=tf.float32) / 255.
 y = tf.convert_to_tensor(y, dtype=tf.int32) 
 
@@ -24,11 +26,12 @@ y = tf.convert_to_tensor(y, dtype=tf.int32)
 x_test = tf.convert_to_tensor(x_test, dtype=tf.float32) / 255.
 y_test = tf.convert_to_tensor(y_test, dtype=tf.int32) 
 
+# 变成可迭代的,分组批量化
 train_db = tf.data.Dataset.from_tensor_slices((x,y))
-train_db = train_db.shuffle(1000).map(proprocess).batch(128)
+train_db = train_db.shuffle(1000).map(proprocess).batch(256)
 
 val_db = tf.data.Dataset.from_tensor_slices((x_test,y_test))
-val_db = val_db.shuffle(1000).map(proprocess).batch(128)
+val_db = val_db.shuffle(1000).map(proprocess).batch(256)
 
 x,y = next(iter(train_db))
 print(x.shape, y.shape)
@@ -64,6 +67,7 @@ network = Sequential([layers.Dense(256, activation='relu'),
                      layers.Dense(64, activation='relu'),
                      layers.Dense(32, activation='relu'),
                      layers.Dense(10)])
+# model建好之后,build一下, 输入 input_shape=
 network.build(input_shape=(4, 28*28))
 network.summary()
 
@@ -72,6 +76,7 @@ network.summary()
 # 导入优化器，损失函数模块
 from tensorflow.keras import optimizers,losses 
 # 采用Adam优化器，学习率为0.01;采用交叉熵损失函数，包含Softmax
+# model compile的时候,输入 optimizer,loss,metrics
 network.compile(optimizer=optimizers.Adam(lr=0.01),
         loss=losses.CategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'] # 设置测量指标为准确率
@@ -80,6 +85,9 @@ network.compile(optimizer=optimizers.Adam(lr=0.01),
 
 #%%
 # 指定训练集为db，验证集为val_db,训练5个epochs，每2个epoch验证一次
+# fit的时候输入  训练数据,epochs,验证数据,验证频率
+# 返回一个history,记录信息
+# 这里validation_data 采用的是test_data,理论上来说不可以,validation应该来自于部分train,会造成一定的误解
 history = network.fit(train_db, epochs=5, validation_data=val_db, validation_freq=2)
 
 
